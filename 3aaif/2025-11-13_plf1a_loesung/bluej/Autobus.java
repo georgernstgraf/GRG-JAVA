e3a0643f5ee93669b5208d792bbc9b00be387466
Georg Graf (SPG) <grafg@spengergasse.at>
2025-11-13 17:10:42 +0100

    challenge istVoll()

--- 3aaif/2025-11-13_plf1a_loesung/bluej/Autobus.java
+++ 3aaif/2025-11-13_plf1a_loesung/bluej/Autobus.java
@@ -15,12 +15,41 @@ class Autobus {
     // 2) die Person auf den ersten freien Sitz setzten und "true" zurückgeben
     // 3) wenn kein Platz mehr frei ist, nix machen und "false" zurückgeben
     public boolean einsteigen(Person person) {
+        if (person == null) {
+            throw new IllegalArgumentException("null sollte nie einsteigen");
+        }
+        if (istDrin(person)) {
+            throw new IllegalArgumentException("null sollte nie einsteigen");
+        }
+        for (int i = 0; i < sitze.length; i++ ) {
+          /*  if (sitze[i] == null) { // also Sitz ist frei
+                sitze[i] = person;
+                return true;
+            } */
+            if (sitze[i] != null) continue;
+            // sitze[i] ist sicher null wenn ich hier lande
+            sitze[i] = person;
+            return true;
+        }
         return false;
     }
 
     // gibt zurück, ob die Person im Bus ist ( true / false )
-    // throws on NULL
     public boolean istDrin(Person person) {
+        for (Person p: sitze) {
+            if (p == person) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean istVoll () {
+        // TODO implement
+        return false;
+    }
+    public boolean hatPlatz () {
+        // TODO
         return false;
     }
 }
