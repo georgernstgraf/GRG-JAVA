bd674692e1b9a23bfa59c427f2db6c9111ff8f68
Georg Graf (SPG) <grafg@spengergasse.at>
2025-12-18 18:22:48 +0100

    Distanzmatrix

--- 3aaif/matrizen/src/main/java/at/spengergasse/Main.java
+++ 3aaif/matrizen/src/main/java/at/spengergasse/Main.java
@@ -6,23 +6,30 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        Matrix a = new Matrix(new int[][]{
-                {0, 1, 0, 0, 1},
-                {1, 0, 1, 1, 1},
-                {0, 1, 0, 0, 0},
-                {0, 1, 0, 0, 1},
-                {1, 1, 0, 1, 0}
+        Matrix a = new Matrix(new Integer[][]{
+                {0, 1, 1, 1, 0},
+                {1, 0, 0, 1, 1},
+                {1, 0, 0, 1, 0},
+                {1, 1, 1, 0, 0},
+                {0, 1, 0, 0, 0}
         });
-        System.out.println(a.getKnotengrade());  // Beispiel: [2, 4, 1, 2, 3]
+        /*for (int i: a.getKnotengrade()) {
+            System.out.print(i + " ");
+        }
+        System.out.println( );*/
+        System.out.println("Matrix:");
+        System.out.println(a);
+        System.out.println("Distanzmatrix:");
+        System.out.println(a.getDistanzMatrix());
     }
 
     public static void mainAlt(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        Matrix a = new Matrix(new int[][]{
+        Matrix a = new Matrix(new Integer[][]{
                 {1, 2}, {3, 4}, {5, 6}, {7, 8}
         });
-        Matrix b = new Matrix(new int[][]{
+        Matrix b = new Matrix(new Integer[][]{
                 {1, 2, 3},
                 {4, 5, 6}
         });
--- 3aaif/matrizen/src/main/java/at/spengergasse/Matrix.java
+++ 3aaif/matrizen/src/main/java/at/spengergasse/Matrix.java
@@ -1,9 +1,9 @@
 package at.spengergasse;
 
 public class Matrix {
-    private int[][] array;
+    private Integer[][] array;
 
-    public Matrix(int[][] array) {
+    public Matrix(Integer[][] array) {
         this.array = array;
     }
 
@@ -15,14 +15,73 @@ public class Matrix {
         return array[0].length;
     }
 
-    public Matrix p2 () {
+    private Integer[][] getArray() {
+        return array;
+    }
+
+    public Matrix getDistanzMatrix() {
+        Integer[][] distanzArray = new Integer[getRows()][getCols()];
+        for (int i = 0; i < getRows(); i++) {
+            for (int j = 0; j < getCols(); j++) {
+                if (i == j) {
+                    distanzArray[i][j] = 0;
+                    continue;
+                }
+                if (array[i][j] != 0) {
+                    distanzArray[i][j] = 1;
+                    continue;
+                }
+                distanzArray[i][j] = null; // Unendliche Distanz
+            }
+        }
+        for (int k = 2; k < getRows(); k++) {
+            if (noInfinityIn(distanzArray)) break;
+            updateDistanzMatrixFrom(distanzArray, p(k).getArray(),k);
+        }
+        return new Matrix(distanzArray);
+    }
+    private static void updateDistanzMatrixFrom(Integer[][] distanzMatrix, Integer[][] potenzMatrix, int weglaenge) {
+        for (int zeile = 0; zeile < distanzMatrix.length ; zeile++) {
+            for (int spalte=0;spalte < distanzMatrix[zeile].length;spalte++) {
+                if (distanzMatrix[zeile][spalte] != null) continue;
+                // Hier ist infinity, evtl. durch Potenzmatrix upzudaten!!
+                if (potenzMatrix[zeile][spalte] > 0) {
+                    distanzMatrix[zeile][spalte] = weglaenge;
+                }
+            }
+        }
+    }
+    private static boolean noInfinityIn(Integer[][] testArray) {
+        for (Integer[] zeile: testArray) {
+            for (Integer spalte: zeile) {
+                if (spalte == null) return false;
+            }
+        }
+        return true;
+    }
+    public int[] getKnotengrade() {
+        if (getRows() != getCols()) {
+            throw new IllegalStateException("Sorry, ist nur für quadratische Matrizen definiert.");
+        }
+        int[] result = new int[getCols()];
+        for (int zeile = 0; zeile < getRows(); zeile++) {
+            // zeile ... aktueller Knoten
+            for (int spalte = 0; spalte < getCols(); spalte++) {
+                result[zeile] += array[zeile][spalte];
+            }
+        }
+        return result;
+    }
+
+    public Matrix p2() {
         return multiply(this);
     }
+
     public Matrix p(int n) {
         if (n < 1) throw new IllegalArgumentException("sorry weiss nicht wie ich das tun soll");
         if (n == 1) return this;
         Matrix result = multiply(this); // dzt. result = this^2
-        for (int i = 2; i < n; i++ ) {
+        for (int i = 2; i < n; i++) {
             result = result.multiply(this);
         }
         return result;
@@ -33,7 +92,12 @@ public class Matrix {
             throw new IllegalArgumentException("Incompatible matrix sizes for multiplication.");
         }
 
-        int[][] result = new int[this.getRows()][other.getCols()];
+        Integer[][] result = new Integer[this.getRows()][other.getCols()];
+        for (Integer[] zeile: result) {
+            for (int spalte = 0; spalte < zeile.length; spalte++){
+                zeile[spalte] = 0;
+            }
+        }
 
         for (int i = 0; i < this.getRows(); i++) {
             for (int j = 0; j < other.getCols(); j++) {
@@ -48,7 +112,7 @@ public class Matrix {
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (int[] row : array) {
+        for (Integer[] row : array) {
             sb.append("[");
             for (int val : row) {
                 sb.append(val).append(" ");
